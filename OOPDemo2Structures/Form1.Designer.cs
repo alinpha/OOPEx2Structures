7ae18ced08fb4899385e8f982635b40f694814e6
Aline Vetrov <alinpha@gmail.com>
2020-01-15 09:51:42 -0800

    init

---
+++ OOPDemo2Structures/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace OOPDemo2Structures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtExt = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ext:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Salary:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(187, 35);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(288, 31);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(187, 75);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(288, 31);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // txtExt
+            // 
+            this.txtExt.Location = new System.Drawing.Point(187, 115);
+            this.txtExt.Name = "txtExt";
+            this.txtExt.Size = new System.Drawing.Size(288, 31);
+            this.txtExt.TabIndex = 2;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(187, 155);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(288, 31);
+            this.txtSalary.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(31, 220);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(117, 49);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(187, 220);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(288, 49);
+            this.btnShow.TabIndex = 5;
+            this.btnShow.Text = "Show Employees";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtExt);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtExt;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ OOPDemo2Structures/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Author: Aline Vetrov
+ * Date: 01/13/2020
+ * Desc: OOP Demo Structures
+ * Notes: Structures are UDT and are value type
+ */
+
+namespace OOPDemo2Structures
+{
+    /*
+     * Employee Structure creation
+     */
+
+    /// <summary>
+    /// Structure of an Employee
+    /// </summary>
+    public struct Employee //pascal case for structure names
+    {
+        public string FirstName; //pascal case for mamber names
+        public string LastName;
+        public long Extension;
+        public decimal Salary;
+    }
+
+    public partial class Form1 : Form
+    {
+        private Employee employee;
+        private ArrayList allEmployees = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                employee.FirstName = txtFirstName.Text.Trim();
+                employee.LastName = txtLastName.Text.Trim();
+                employee.Extension = long.Parse(txtExt.Text);
+                employee.Salary = decimal.Parse(txtSalary.Text);
+
+                allEmployees.Add(employee);
+
+                MessageBox.Show("Employee added to arraylist");
+
+                foreach(Control ctr in this.Controls)
+                {
+                    if (ctr is TextBox)
+                    {
+                        ctr.ResetText();
+                    }
+                }
+
+                txtFirstName.Focus();
+
+                if (allEmployees.Count % 5 == 0)
+                {
+                    printAllEmployees();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            printAllEmployees();
+        }
+
+        private void printAllEmployees()
+        {
+            string mssg = "";
+
+            foreach (Employee emp in allEmployees)
+            {
+                mssg += $"{emp.FirstName} {emp.LastName} {emp.Extension} {emp.Salary.ToString("c")} {Environment.NewLine}";
+            }
+
+            MessageBox.Show(mssg);
+        }
+    }
+}
